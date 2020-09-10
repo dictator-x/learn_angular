e58deb83c3d13573c53407445e5954495daf663f
dictator-x <dictator.xwu@gmail.com>
2020-09-09 20:56:19 -0400

    format

--- music-player/src/app/share/share.module.ts
+++ music-player/src/app/share/share.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { NgZorroAntdModule } from 'ng-zorro-antd'
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common'
 
 
 
 @NgModule({
   declarations: [],
   imports: [
+    CommonModule
     FormsModule,
     NgZorroAntdModule
   ],
