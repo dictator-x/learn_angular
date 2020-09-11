1ea701517b22e5c9c339660faa7cee773745c4f9
dictator-x <dictator.xwu@gmail.com>
2020-09-11 17:55:07 -0400

    format code

--- music-player/src/app/page/home/components/carousel/carousel.component.ts
+++ music-player/src/app/page/home/components/carousel/carousel.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import type { TemplateRef } from '@angular/core';
 
 @Component({
   selector: 'app-carousel',
