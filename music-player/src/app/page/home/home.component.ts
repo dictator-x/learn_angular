e6a0aff4437d2ecf5247b5be544d634a54c7499c
dictator-x <dictator.xwu@gmail.com>
2020-09-11 13:56:24 -0400

    home service

---
+++ music-player/src/app/page/home/home.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+import { HomeService } from 'src/app/service/home.service'
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.less']
+})
+export class HomeComponent implements OnInit {
+
+  constructor(
+    private homeService: HomeService
+  ){}
+
+  ngOnInit(): void {
+    this.homeService.getBanners().subscribe(banners => {
+      console.log(banners)
+    });
+  }
+
+}
