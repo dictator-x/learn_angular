be7126da941ca0c0b415fc211bc43d8de712bab8
dictator-x <dictator.xwu@gmail.com>
2020-09-12 17:59:04 -0400

    music player ui

---
+++ music-player/src/app/share/ui/music-player/music-player.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { MusicPlayerComponent } from './music-player.component';
+
+
+
+@NgModule({
+  declarations: [MusicPlayerComponent],
+  imports: [
+  ],
+  exports: [
+    MusicPlayerComponent
+  ]
+})
+export class MusicPlayerModule { }
--- music-player/src/app/share/ui/ui.module.ts
+++ music-player/src/app/share/ui/ui.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 
 import { SongCardComponent } from './song-card/song-card.component';
 import { CountNumberFormatterPipe } from '../pipe/count-number-formatter.pipe'
+import { MusicPlayerModule } from './music-player/music-player.module'
 
 
 
@@ -13,7 +14,8 @@ import { CountNumberFormatterPipe } from '../pipe/count-number-formatter.pipe'
   imports: [
   ],
   exports: [
-    SongCardComponent
+    SongCardComponent,
+    MusicPlayerModule
   ]
 })
 export class UiModule { }
