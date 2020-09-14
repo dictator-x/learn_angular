9fe9c0eb4caf076600ad5377171ecb93f6a892d1
dictator-x <dictator.xwu@gmail.com>
2020-09-14 17:45:24 -0400

    choose next and pre music

--- music-player/src/app/share/ui/music-player/music-player.component.ts
+++ music-player/src/app/share/ui/music-player/music-player.component.ts
@@ -1,16 +1,17 @@
 import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { Store, select } from '@ngrx/store';
 
-import { AppStoreModule } from 'src/app/store'
+import { AppStoreModule } from 'src/app/store';
 import {
   getSongList,
   getPlayList,
   getCurrentSong,
   getPlayMode,
   getCurrentIndex
-} from 'src/app/store/selectors/player.selector'
-import { Song } from 'src/app/data-types/common.types'
-import { PlayMode } from './player.types'
+} from 'src/app/store/selectors/player.selector';
+import { Song } from 'src/app/data-types/common.types';
+import { PlayMode } from './player.types';
+import { setCurrentIndex } from 'src/app/store/actions/player.actions';
 
 @Component({
   selector: 'app-music-player',
@@ -44,11 +45,11 @@ export class MusicPlayerComponent implements OnInit, AfterViewInit {
     const stateArr: any[] = [
       {
         type: getSongList,
-        cb: list => this.watchList(list, 'songlist')
+        cb: list => this.watchList(list, 'songList')
       },
       {
         type: getPlayList,
-        cb: list => this.watchList(list, 'playlist')
+        cb: list => this.watchList(list, 'playList')
       },
       {
         type: getCurrentIndex,
@@ -93,20 +94,50 @@ export class MusicPlayerComponent implements OnInit, AfterViewInit {
   }
 
   public onCanPlay(): void {
+    console.log("aa")
     this.songReady = true;
     this.play();
   }
 
   public onToggle(): void {
-    if ( this.songReady ) {
-      this.playing = ! this.playing;
-      this.playing ? this.audioEl.play() : this.audioEl.pause();
+    if ( ! this.currentSong ) {
+      if ( this.playList.length ) {
+        this.store.dispatch(setCurrentIndex({ currentIndex: 0 }))
+        this.songReady = false;
+      }
+    } else {
+      if ( this.songReady ) {
+        this.playing = ! this.playing;
+        this.playing ? this.audioEl.play() : this.audioEl.pause();
+      }
     }
+  }
+
+  public onPrev(): void {
+    if ( ! this.songReady ) return;
+    const newIndex = this.currentIndex === 0 ? this.playList.length - 1 : this.currentIndex - 1;
+    this.updateIndex(newIndex);
+  }
+
+  public onNext(): void {
+    if ( ! this.songReady ) return;
+    const newIndex = this.currentIndex === this.playList.length - 1 ? 0 : this.currentIndex + 1;
+    this.updateIndex(newIndex);
+  }
+
+  private updateIndex(index: number): void {
+    this.store.dispatch(setCurrentIndex({ currentIndex: index }));
+    this.songReady = false;
   }
 
   private play(): void {
     this.audioEl.play();
     this.playing = true;
+  }
+
+  private loop(): void {
+    this.audioEl.currentTime = 0;
+    this.play();
   }
 
   get picUrl(): string {
