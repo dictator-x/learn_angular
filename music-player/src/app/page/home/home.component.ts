5bd0429484c5032963cf95140ffe6c53e4af5270
dictator-x <dictator.xwu@gmail.com>
2020-09-18 14:11:38 -0400

    recover home.component.ts

--- music-player/src/app/page/home/home.component.ts
+++ music-player/src/app/page/home/home.component.ts
@@ -4,20 +4,16 @@ import { NzCarouselComponent } from 'ng-zorro-antd';
 import { map } from 'rxjs/internal/operators';
 import { Store } from '@ngrx/store';
 
-import { AppStoreModule } from 'src/app/store';
+import { AppStoreModule } from 'src/app/store'
 import { SheetService } from 'src/app/service/sheet.service';
 import { setPlayList, setSongList, setCurrentIndex } from 'src/app/store/actions/player.actions';
-import { select } from '@ngrx/store';
-import { getPlayerState } from 'src/app/store/selectors/player.selector';
 
 import type {
   Banner,
   HotTag,
   SongSheet,
-  Singer,
+  Singer
 } from 'src/app/data-types/common.types';
-import { PlayState } from 'src/app/store/reducers/player.reducer';
-import { shuffle, findIndex } from 'src/app/util/array';
 
 @Component({
   selector: 'app-home',
@@ -31,8 +27,6 @@ export class HomeComponent implements OnInit {
   public songSheets: SongSheet[] = [];
   public singers: Singer[] = [];
   public carouselActiveIndex: number = 0;
-
-  private playerState: PlayState;
 
   @ViewChild(NzCarouselComponent, {static: true})
   private nzCarousel: NzCarouselComponent;
@@ -54,8 +48,6 @@ export class HomeComponent implements OnInit {
         this.singers = singers;
       }
     );
-
-    this.store.pipe(select(getPlayerState)).subscribe((res: PlayState) => this.playerState = res);
   }
 
   public onBeforeChange(event: any): void {
@@ -71,17 +63,8 @@ export class HomeComponent implements OnInit {
     this.sheetService.playSheet(id)
     .subscribe((list) => {
       this.store.dispatch(setSongList({ songList: list }));
-
-      let index = 0;
-      let tmpList = list.slice();
-
-      if ( this.playerState.playMode.type === 'random' ) {
-        tmpList = shuffle(list || []);
-        index = findIndex(tmpList, list[index])
-      }
-
-      this.store.dispatch(setPlayList({ playList: tmpList }));
-      this.store.dispatch(setCurrentIndex({ currentIndex: index }));
+      this.store.dispatch(setPlayList({ playList: list }));
+      this.store.dispatch(setCurrentIndex({ currentIndex: 0 }));
     });
   }
 }
---
+++ music-player/src/app/store/batch-actions.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+import { AppStoreModule } from './index';
+
+@Injectable({
+  providedIn: 'AppStoreModule'
+})
+export class BatchActionsService {
+
+
+  selectPlayList({ list, index })
+}
